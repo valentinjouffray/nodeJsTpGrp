cfe1393aa501f4e713e631748ba4784386b0c895
Calcei Pamela <128471857+PamelaCalcei@users.noreply.github.com>
2024-06-13 14:52:52 +0200

    commande controller updated

--- src/controllers/commandeController.js
+++ src/controllers/commandeController.js
@@ -33,16 +33,17 @@ commandeController.create = (req, res) => {
 
   const { prix, date, status } = req.form;
   const commande = { prix, date, status };
+  if(new Date(commande.date) < new Date()){
+    res.status(400).send({error : "Invalid date"})
+  }
+  Commande.create(commande)
+    .then((commande) => {
+      return res.status(201).send({ commande, message: "Commande created !" });
+    })
+    .catch((error) => {
+      res.status(400).send({ message: "Failed creating commande", error });
+    });
 };
-
-Commande.create(commande)
-  .then((commande) => {
-    return res.status(201).send({ commande, message: "Commande created !" });
-  })
-  .catch((error) => {
-    res.status(400).send({ message: "Failed creating commande", error });
-  });
-
 commandeController.update = (req, res) => {
   if (!req.form.isValid) {
     res.status(400).json({ message: "Invalid Form" });
@@ -51,6 +52,19 @@ commandeController.update = (req, res) => {
   const id = req.params.id;
   const { prix, date, status } = req.form;
   const commande = { prix, date, status };
+
+  Commande.findByPk(id)
+    .then((commande) => {
+      if (!commande) {
+        return res.send({ message: "Commande not foud" });
+      } else if (commande.status === "TERMINE") {
+        throw new Error("Can't update commande with status TERMINE");
+      }
+      return res.status(200).send(commande);
+    })
+    .catch((error) => {
+      res.status(400).send({ message: "Failed fetching commande", error });
+    });
 
   Commande.update(commande, { where: { id } })
     .then((commande) => {
