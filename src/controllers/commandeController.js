23e235541af54d4267e1118bab8c89df41e0904a
Calcei Pamela <128471857+PamelaCalcei@users.noreply.github.com>
2024-06-13 13:58:21 +0200

    Fix commande

--- src/controllers/commandeController.js
+++ src/controllers/commandeController.js
@@ -31,46 +31,46 @@ commandeController.create = (req, res) => {
     res.status(400).json({ message: "Invalid Form" });
   }
 
-  const {prix, date ,status} = req.form
-  const commande = {prix, date ,status}
+  const { prix, date, status } = req.form;
+  const commande = { prix, date, status };
 };
 
 Commande.create(commande)
-.then((commande)=>{
-    return res.status(201).send({ commande, message : "Commande created !" })
-}).catch((error) => {
-  res.status(400).send({message : "Failed creating commande", error})
-})
-
+  .then((commande) => {
+    return res.status(201).send({ commande, message: "Commande created !" });
+  })
+  .catch((error) => {
+    res.status(400).send({ message: "Failed creating commande", error });
+  });
 
 commandeController.update = (req, res) => {
+  if (!req.form.isValid) {
+    res.status(400).json({ message: "Invalid Form" });
+  }
 
-if (!req.form.isValid){
-  res.status(400).json({ message : "Invalid Form"})
-}
+  const id = req.params.id;
+  const { prix, date, status } = req.form;
+  const commande = { prix, date, status };
 
-const id = req.params.id
-const {prix, date ,status} = req.form
-const commande = { prix, date ,status }
-
-Commande.update(commande, { where : { id }})
-.then( (commande) => {
-  return res.status(200).send({ commande, message : "Commande updated !" })
-}).catch((error) => {
-  res.status(400).send({message : "Failed updating commande", error})
-})
-}
+  Commande.update(commande, { where: { id } })
+    .then((commande) => {
+      return res.status(200).send({ commande, message: "Commande updated !" });
+    })
+    .catch((error) => {
+      res.status(400).send({ message: "Failed updating commande", error });
+    });
+};
 
 controller.delete = (req, res) => {
-const id = req.params.id
+  const id = req.params.id;
 
-Commande.destroy({ where : { id }})
-.then( (commande) => {
-  return res.status(200).send({ commande, message : "Commande deleted !" })
-}).catch((error) => {
-  res.status(400).send({message : "Failed deleting commande", error})
-})
-}
+  Commande.destroy({ where: { id } })
+    .then((commande) => {
+      return res.status(200).send({ commande, message: "Commande deleted !" });
+    })
+    .catch((error) => {
+      res.status(400).send({ message: "Failed deleting commande", error });
+    });
+};
 
-
-module.exports = commandeController
+module.exports = commandeController;
