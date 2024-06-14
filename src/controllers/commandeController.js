42d7c5f3034a4c449839cae9e200927a55cc81d3
Valentin JOUFFRAY <valentin.jouffray@gmail.com>
2024-06-14 12:22:05 +0200

    added commande endpoints

--- src/controllers/commandeController.js
+++ src/controllers/commandeController.js
@@ -96,4 +96,62 @@ commandeController.delete = (req, res) => {
     });
 };
 
+commandeController.addBiereToCommande = async (req, res) => {
+  const { commandeId, biereId } = req.params;
+
+  try {
+    const commande = await Commande.findByPk(commandeId);
+
+    if (!commande) {
+      return res.status(404).send({ error: "Commande not found" });
+    }
+
+    if (commande.status === "TERMINE") {
+      return res
+        .status(400)
+        .send({ error: "Can't update commande with status TERMINE" });
+    }
+
+    await commande.addBiere(biereId);
+
+    return res.status(200).send({
+      message: "Biere added to commande",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      error: "Failed processing request",
+      errors: error.errors ? error.errors.map((e) => e.message) : error.name,
+    });
+  }
+};
+
+commandeController.removeBiereFromCommande = async (req, res) => {
+  const { commandeId, biereId } = req.params;
+
+  try {
+    const commande = await Commande.findByPk(commandeId);
+
+    if (!commande) {
+      return res.status(404).send({ error: "Commande not found" });
+    }
+
+    if (commande.status === "TERMINE") {
+      return res
+        .status(400)
+        .send({ error: "Can't update commande with status TERMINE" });
+    }
+
+    await commande.removeBiere(biereId);
+
+    return res.status(200).send({
+      message: "Biere removed from commande",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      error: "Failed processing request",
+      errors: error.errors ? error.errors.map((e) => e.message) : error.name,
+    });
+  }
+};
+
 module.exports = commandeController;
--- src/router/commandeRouter.js
+++ src/router/commandeRouter.js
@@ -8,5 +8,9 @@ commandeRouter.post("/", commandeController.create);
 commandeRouter.put("/:id", commandeController.update);
 commandeRouter.delete("/:id", commandeController.delete);
 commandeRouter.get("/:id", commandeController.getById);
+//   POST /commandes/:commandeId/biere/:biereId => Ajouter une bière à une commande
+commandeController.post("/commandes/:commandeId/biere/:biereId", commandeController.addBiereToCommande);
+//   DELETE /commandes/:commandeId/biere/:biereId => Supprimer une bière d'une commande
+commandeController.delete("/commandes/:commandeId/biere/:biereId", commandeController.removeBiereFromCommande);
 
 module.exports = commandeRouter;
